864bced2a3895fbd35fd8c0cb107c76f76af398b
tomaslauda <lauda.to.2020@skola.ssps.cz>
2023-04-16 00:09:51 +0200

    1642023

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,7 +1,8 @@
 
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import LinkWeather from './components/linkWeather.vue';
+import LinkWeather from './components/linkWeather.vue' 
+import RightHead from './components/rightHead.vue';
 </script>
 
 <template>
@@ -16,26 +17,32 @@ import LinkWeather from './components/linkWeather.vue';
   <div class="wrapper">
     <div class="leftContainer">
       <LinkWeather name="Praha" temparature="21" />
-      <LinkWeather name="Brno" temparature="21" />
+      <LinkWeather name="Brno" temparature="21"/>
       <LinkWeather name="Ostrava" temparature="21" />
-      <LinkWeather name="Olomouc" temparature="21" />
+      <LinkWeather name="Olomouc" temparature="21"  />
     </div>
     <div class="middleContainer">
       <LinkWeather name="Praha" temparature="21" />
-      <LinkWeather name="Brno" temparature="21" />
-      <LinkWeather name="Ostrava" temparature="21" />
-      <LinkWeather name="Olomouc" temparature="21" />
+      <LinkWeather name="Brno" temparature="21" region="fagd"/>
+      <LinkWeather name="Ostrava" temparature="21" region="fagd"/>
+      <LinkWeather name="Olomouc" temparature="21" region="fagd"/>
     </div>
     <div class="rightContainer">
-      <LinkWeather name="Praha" temparature="21" />
-      <LinkWeather name="Brno" temparature="21" />
-      <LinkWeather name="Ostrava" temparature="21" />
-      <LinkWeather name="Olomouc" temparature="21" />
+      <RightHead head1="<" head2="Head" head3=">"/>
+      <LinkWeather name="Praha" temparature="21" image1="https://png.pngtree.com/png-clipart/20201029/ourmid/pngtree-circle-clipart-blue-circle-png-image_2381949.jpg" image2="https://www.vhv.rs/dpng/d/443-4431349_yellow-circle-png-transparent-png-download.png"/>
+      <LinkWeather name="Brno" temparature="13" image1="https://png.pngtree.com/png-clipart/20201029/ourmid/pngtree-circle-clipart-blue-circle-png-image_2381949.jpg" image2="https://www.vhv.rs/dpng/d/443-4431349_yellow-circle-png-transparent-png-download.png"/>
+      <LinkWeather name="Ostrava" temparature="18" image1="https://png.pngtree.com/png-clipart/20201029/ourmid/pngtree-circle-clipart-blue-circle-png-image_2381949.jpg" image2="https://www.vhv.rs/dpng/d/443-4431349_yellow-circle-png-transparent-png-download.png"/>
+      <LinkWeather name="Olomouc" temparature="21" image1="https://png.pngtree.com/png-clipart/20201029/ourmid/pngtree-circle-clipart-blue-circle-png-image_2381949.jpg" image2="https://www.vhv.rs/dpng/d/443-4431349_yellow-circle-png-transparent-png-download.png"/>
+      <LinkWeather name="Liberec" temparature="14" image1="https://png.pngtree.com/png-clipart/20201029/ourmid/pngtree-circle-clipart-blue-circle-png-image_2381949.jpg" image2="https://www.vhv.rs/dpng/d/443-4431349_yellow-circle-png-transparent-png-download.png"/>
+      <LinkWeather name="Karlovy Vary" temparature="21" image1="https://png.pngtree.com/png-clipart/20201029/ourmid/pngtree-circle-clipart-blue-circle-png-image_2381949.jpg" image2="https://www.vhv.rs/dpng/d/443-4431349_yellow-circle-png-transparent-png-download.png"/>
+      <LinkWeather name="J. Hradec" temparature="13" image1="https://png.pngtree.com/png-clipart/20201029/ourmid/pngtree-circle-clipart-blue-circle-png-image_2381949.jpg" image2="https://www.vhv.rs/dpng/d/443-4431349_yellow-circle-png-transparent-png-download.png"/>
+      <LinkWeather name="Jihlava" temparature="12" image1="https://png.pngtree.com/png-clipart/20201029/ourmid/pngtree-circle-clipart-blue-circle-png-image_2381949.jpg" image2="https://www.vhv.rs/dpng/d/443-4431349_yellow-circle-png-transparent-png-download.png"/>
+      <LinkWeather name="Chomutov" temparature="21" image1="https://png.pngtree.com/png-clipart/20201029/ourmid/pngtree-circle-clipart-blue-circle-png-image_2381949.jpg" image2="https://www.vhv.rs/dpng/d/443-4431349_yellow-circle-png-transparent-png-download.png"/>
     </div>
   </div>
 </template>
 <style>
-.middleContainer{
+.MiddleContainer{
   width: 49%;
 }
 .wrapper {
@@ -51,4 +58,5 @@ import LinkWeather from './components/linkWeather.vue';
   width: 25%;
   background-color: lightgray;
 }
+
 </style>
