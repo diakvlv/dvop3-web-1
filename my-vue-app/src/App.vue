6b9f9e3dfb7d48ad101cb9d0009e51e018364aa8
diakvlv <112659334+diakvlv@users.noreply.github.com>
2023-04-23 22:15:13 +0200

    Commit Instasamka

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,22 +1,53 @@
 <script setup>
-import leftPanel from './components/leftPanel.vue'
 </script>
 
 <template>
-  <leftPanel/>
+  <!--<div>
+        <a href="https://vitejs.dev/" target="_blank">
+                <img src="/vite.svg" class="logo" alt="Vite logo" />
+              </a>
+              <a href="https://vuejs.org/" target="_blank">
+                <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+              </a>
+            </div>-->
+  <div class="wrapper">
+    <div class="leftContainer">
+      LEVÝ
+    </div>
+    <div class="middleContainer">
+      PROSTŘEDNÍ
+    </div>
+    <div class="rightContainer">
+      PRAVÝ
+    </div>
+  </div>
 </template>
+<style>
+.prev {
+  display: flex;
+    gap: 20px;
+    justify-content: space-between;
+    padding: 0 20px;
+}
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.middleContainer {
+  width: 49%;
+  background-color: lightgray
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.rightContainer {
+  background-color: lightgray;
+  width: 25%;
+}
+
+.leftContainer {
+  width: 25%;
+  background-color: lightgray;
 }
 </style>
